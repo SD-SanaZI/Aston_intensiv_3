8df7d68723f498be62b1214ede400404689ac73c
Drozd_AS <31595916+SD-SanaZI@users.noreply.github.com>
2024-03-06 10:16:18 +0300

    fix Serializable

--- app/src/main/java/com/example/aston_intensiv_3/DataChangeDialog.kt
+++ app/src/main/java/com/example/aston_intensiv_3/DataChangeDialog.kt
@@ -3,13 +3,14 @@ package com.example.aston_intensiv_3
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.example.aston_intensiv_3.data.PersonData
 import com.example.aston_intensiv_3.data.TelephoneData
-import com.google.gson.Gson
+import java.lang.Exception
 
 const val TELEPHONE_DATA_KEY = "TELEPHONE_DATA_KEY"
 
@@ -23,7 +24,8 @@ class DataChangeDialog: DialogFragment() {
         fun newInstance(data: TelephoneData): DataChangeDialog{
             val dialog = DataChangeDialog()
             val bundle = Bundle()
-            bundle.putString(TELEPHONE_DATA_KEY,Gson().toJson(data))
+            bundle.putSerializable(TELEPHONE_DATA_KEY,data)
+            //bundle.putString(TELEPHONE_DATA_KEY,Gson().toJson(data))
             dialog.arguments = bundle
             return dialog
         }
@@ -32,7 +34,14 @@ class DataChangeDialog: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val view = layoutInflater.inflate(R.layout.dialog, null)
-            val data: TelephoneData = Gson().fromJson(arguments?.getString(TELEPHONE_DATA_KEY),TelephoneData::class.java)
+            val bundle = arguments as Bundle
+            val data: TelephoneData = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                bundle.getSerializable(TELEPHONE_DATA_KEY,TelephoneData::class.java)
+                    ?: throw Exception("Null Dialog data")
+            } else {
+                bundle.getSerializable(TELEPHONE_DATA_KEY) as TelephoneData
+            }
+            //Gson().fromJson(arguments?.getString(TELEPHONE_DATA_KEY),TelephoneData::class.java)
             val name = view.findViewById<EditText>(R.id.nameEdit)
             val surname = view.findViewById<EditText>(R.id.surnameEdit)
             val telephoneNumber = view.findViewById<EditText>(R.id.telephoneNumberEdit)
